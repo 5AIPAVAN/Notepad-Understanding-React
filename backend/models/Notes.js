46c812eb5225c0548be32a5fc0e9e17c3a51eca4
5AIPAVAN <123063767+5AIPAVAN@users.noreply.github.com>
2024-09-25 23:15:38 +0530

    addnote,fetc all notes function added

--- backend/models/Notes.js
+++ backend/models/Notes.js
@@ -1,23 +1,27 @@
 const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
-const NoteSchema = new Schema({
-    title: {
+const NotesSchema = new Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    title:{
         type: String,
         required: true
     },
-    description: {
+    description:{
         type: String,
-        required: true
+        required: true, 
     },
-    tag: {
+    tag:{
         type: String,
-        default:"General"
+        default: "General"
     },
-    date: {
+    date:{
         type: Date,
         default: Date.now
     },
-})
+  });
 
-module.exports = mongoose.model('note',NoteSchema);
+  module.exports = mongoose.model('notes', NotesSchema);
